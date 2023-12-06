b46d5ae0cf0c49824fa08a0d831c16a89b7abf01
Lérè <lere.williams@gmail.com>
2023-12-06 13:23:48 -0800

    Retrieve enough git history to compute local diff

--- src/initializer.ts
+++ src/initializer.ts
@@ -30,15 +30,68 @@ export function loadConfiguration(): Configuration {
  * Retrieves the diff of the pull request that triggered this workflow which we
  * will use for evaluation.
  *
+ * The approach taken below was adapted from:
+ * https://github.com/actions/checkout/issues/520#issuecomment-1167205721
+ *
  * @param pull The pull request that triggered this workflow.
  * @returns The diff of the given pull request.
  */
 export async function fetchDiff(pull: PullRequest): Promise<string> {
-  const git = simpleGit()
-  const diffArgs = ['--merge-base', pull.base.sha].concat(
+  const git = simpleGit('.', { trimmed: true })
+  const baseRef = `origin/${pull.base.ref}`
+  const baseRefspec = `+${pull.base.sha}:remotes/${baseRef}`
+  const headRef = `origin/${pull.head.ref}`
+  const headRefspec = `+${pull.head.sha}:remotes/${headRef}`
+  const diffArgs = ['--merge-base', baseRef].concat(
     core.getInput('git-diff-args').split(/\s+/)
   )
+
   core.info(`Retrieving diff with \`git diff ${diffArgs.join(' ')}\``)
+
+  // Fetch all commits for the head branch back to where it diverged from the base.
+  core.debug(`Fetching ${pull.commits + 1} for ${headRefspec}`)
+  await git.fetch([
+    'origin',
+    headRefspec,
+    `--depth=${pull.commits + 1}`,
+    '--no-tags',
+    '--prune',
+    '--no-recurse-submodules'
+  ])
+
+  core.debug(`Switching to branch ${pull.head.ref}`)
+  await git.raw('switch', pull.head.ref)
+
+  // Find a date that is far enough back to contain the history we need.
+  const commonAncestor = await git.raw(
+    'rev-list',
+    '--first-parent',
+    '--max-parents=0',
+    '--max-count=1',
+    headRef
+  )
+  const commonAncestorCommittedAt = await git.show([
+    '--quiet',
+    '--date=iso8601',
+    '--format=%cd',
+    commonAncestor
+  ])
+
+  // Fetch commits for the base branch that were made since the head diverged from it.
+  core.debug(
+    `Retrieving history for ${baseRefspec} since ${commonAncestorCommittedAt}`
+  )
+  await git.fetch([
+    'origin',
+    baseRefspec,
+    `--shallow-since=${commonAncestorCommittedAt}`,
+    '--no-tags',
+    '--prune',
+    '--no-recurse-submodules'
+  ])
+
+  // Now we have all relevant history from both base and head branches, so we
+  // can compute an accurate diff relative to the base branch.
   return git.diff(diffArgs)
 }
 
